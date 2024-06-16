a7d0f901d4d4e03d807fc4691c7c2365b0d321e4
Kerciu <Kerciuuu@gmail.com>
2024-06-16 16:50:40 +0200

    added row serializing and deserializing

--- row.c
+++ row.c
@@ -1,1 +1,15 @@
 #include "row.h"
+
+void serializeRow(Row *source, void *destination)
+{
+    memcpy(source + ID_OFFSET, &(source->id), ID_SIZE);
+    memcpy(source + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+    memcpy(source + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+void deserializeRow(void *source, Row *destination)
+{
+    memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+    memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+    memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+}
--- row.h
+++ row.h
@@ -1,12 +1,23 @@
 #ifndef DATABASE_ROW
 #define DATABASE_ROW
 
+/*
+    For now I will store data in pages
+    later on I will implement B-Trees
+*/
+
 #include <stdlib.h>
 #include <sys/types.h>
 
 #define USERNAME_MAX_LENGTH 32
 #define EMAIL_MAX_LENGTH 256
 #define sizeOFAttribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+
+typedef struct {
+    u_int32_t id;
+    char username[USERNAME_MAX_LENGTH];
+    char email[EMAIL_MAX_LENGTH];
+} Row;
 
 const u_int32_t ID_SIZE = sizeOFAttribute(Row, id);
 const u_int32_t USERNAME_SIZE = sizeOFAttribute(Row, username);
@@ -16,12 +27,7 @@ const u_int32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
 const u_int32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
 const u_int32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
 
-typedef struct {
-    u_int32_t id;
-    char username[USERNAME_MAX_LENGTH];
-    char email[EMAIL_MAX_LENGTH];
-} Row;
-
-
+void serializeRow(Row* source, void* destination);
+void deserializeRow(Row* source, void* destination);
 
 #endif
