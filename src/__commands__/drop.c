cb78b587e927bd6fd32a35c2b0e8b8cb6530d9fe
Kerciu <Kerciuuu@gmail.com>
2024-07-13 12:51:27 +0200

    optimized DROP command

--- src/__commands__/drop.c
+++ src/__commands__/drop.c
@@ -1,10 +1,6 @@
 #include "../__input__/statement.h"
 
 StatementStatus constructDrop(InputBuffer* buffer, Statement* statement) {
-    statement->operationBounds.type = OPERATION_IN_BOUNDS;
-    statement->operationBounds.startIdx = 0;
-    statement->operationBounds.endIdx = INT_MAX;
-
     statement->type = STATEMENT_DROP;
     return CONSTRUCTION_SUCCESS;
 }
@@ -12,39 +8,25 @@ StatementStatus constructDrop(InputBuffer* buffer, Statement* statement) {
 ExecuteStatus executeDrop(Statement* statement, Table* table) {
     void* node = getPage(table->pager, table->rootPageNum);
     uint32_t numCells = *leafNodeNumCells(node);
+    if (numCells < 1) return EXECUTE_SUCCESS_NO_RECORDS;
 
-    uint32_t* startPtr = &(statement->operationBounds.startIdx);
-    *startPtr = (*startPtr >= getTableMinID(table) ? *startPtr : getTableMinID(table));
-    Cursor* startCursor = tableFind(table, *startPtr);
+    uint32_t start = getTableMinID(table);
+    uint32_t end = getTableMaxID(table);
+    for (uint32_t i = start; i <= end; ++i) {
+        printf("Start and end values in a while loop: %d and %d\n", i, end);
+        Cursor* cursor = tableFind(table, i);
+        if (cursor->cellNum < numCells) {
+            uint32_t keyAtIdx = *leafNodeKey(node, cursor->cellNum);
 
-    uint32_t* endPtr = &(statement->operationBounds.endIdx);
-    *endPtr = (*endPtr <= getTableMaxID(table) ? *endPtr : getTableMaxID(table)); 
-    Cursor* endCursor = tableFind(table, *endPtr);
-
-    if (startCursor->cellNum < numCells && endCursor->cellNum < numCells) {
-        for (uint32_t i = *startPtr; i <= *endPtr; ++i) {
-            printf("Start and end values in a while loop: %d and %d\n", i, *endPtr);
-            Cursor* cursor = tableFind(table, i);
-            if (cursor->cellNum < numCells) {
-                uint32_t keyAtIdx = *leafNodeKey(node, cursor->cellNum);
-
-                if (keyAtIdx == i) {
-                    leafNodeDelete(cursor);
-                }
-                else continue;
+            if (keyAtIdx == i) {
+                leafNodeDelete(cursor);
             }
-            else {
-                return EXECUTE_FAILURE;
-            }
+            else continue;
         }
+        else {
+            return EXECUTE_FAILURE;
+        }
+    }
 
-        free(startCursor);
-        free(endCursor);
-        return EXECUTE_SUCCESS;
-    }
-    else {
-        free(startCursor);
-        free(endCursor);
-        return EXECUTE_FAILURE;
-    }
+    return EXECUTE_SUCCESS;
 }
